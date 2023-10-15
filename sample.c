a284e841664a2e3a16588912e480ac0fc2fe0ffd
deepikasingidi <147781264+deepikasingidi@users.noreply.github.com>
2023-10-15 14:13:52 +0530

    Add files via upload

---
+++ sample.c
@@ -0,0 +1,13 @@
+//increment and decrement operators(post:a++,pre:++a)
+//example:post(a++)
+//we can do sudtractions also
+#include<stdio.h>
+int main() {
+    int a;
+    double o;
+    scanf("%d",&a);
+    o=--a;
+    printf("%.2lf",o);
+}
+
+
