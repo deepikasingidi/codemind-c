908337cde2d78151e6380903875ac55b7c449a49
deepikasingidi <147781264+deepikasingidi@users.noreply.github.com>
2023-11-22 14:41:57 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=n; i>0; i--){         for(int j=i; j<=n; j++){             printf("%c ",i+64);         }         printf(" ");     } }

---
+++ Basic_Pattern_2.5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=n; i>0; i--){
+        for(int j=i; j<=n; j++){
+            printf("%c ",i+64);
+        }
+        printf("
+");
+    }
+}
