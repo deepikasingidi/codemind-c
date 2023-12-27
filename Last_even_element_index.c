040fc0bc2e8299c9197e723fb3ad604fcee8d06e
deepikasingidi <147781264+deepikasingidi@users.noreply.github.com>
2023-12-27 09:57:43 +0530

    #include<stdio.h> int main() {     int n,res;     scanf("%d",&n);     int arr[n];     for(int i=0; i<n; i++){         scanf("%d",&arr[i]);         if(arr[i]%2 == 0){             res = i;         }     }     printf("%d",res); }

---
+++ Last_even_element_index.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,res;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d",&arr[i]);
+        if(arr[i]%2 == 0){
+            res = i;
+        }
+    }
+    printf("%d",res);
+}
